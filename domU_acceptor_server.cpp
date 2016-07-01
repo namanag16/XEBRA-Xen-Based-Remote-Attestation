9c073e2d00ef786a8537577350e61b305956086d
naman <namanag16@gmail.com>
2016-07-01 20:23:50 +0530

    initiated structure for allcomms-- verifier to domUserver

--- domU_acceptor_server.cpp
+++ domU_acceptor_server.cpp
@@ -11,6 +11,35 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+#define SERVER_SOCKET_CODE \
+    /*create a socket file descriptor */ \
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); \
+    if (sockfd < 0) \
+       error("ERROR opening socket"); \
+    bzero((char *) &serv_addr, sizeof(serv_addr));    /* set server address bits to zero*/ \
+    portno = atoi(argv[1]); \
+    serv_addr.sin_family = AF_INET;  \
+    serv_addr.sin_addr.s_addr = INADDR_ANY;  /* INADDR_ANY is server ip address*/ \
+    serv_addr.sin_port = htons(portno);   /* htons converts port number in host byte order to a port number in network byte order.*/ \
+    /* bind the server to port and host ... this steps makes it a server socket */ \
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, \
+              sizeof(serv_addr)) < 0)  \
+              error("ERROR on binding"); 
+
+
+typedef unsigned char byte;
+
+struct bundle{
+    int a,b;
+    byte eflag;
+    byte nonce[16];
+    byte content_ab[32];  // fixing the content size to 32 bytes for now
+    byte hmac[256];
+
+};
+
+typedef struct bundle packet;
+
 int block_recv(const int fd, char* data, unsigned int len)
 {
     int i, j = 0;
@@ -34,58 +63,40 @@ void error(const char *msg)
     perror(msg);
     exit(1);
 }
+void fetch_content(packet* p, int a, int b)
+{
+    byte mem[1000];
+    for(int i = 0; i < 1000;i++)
+    {
+        mem[i] = i;
+    }
+
+    for(int i = a;i<=b;i++)
+        p->content_ab[i] = mem[i];
+}
 
 int main(int argc, char *argv[])
 {
     //Declaration section 
     int sockfd, newsockfd, portno;
     socklen_t clilen;
-    unsigned char buffer[1024];
     struct sockaddr_in serv_addr, cli_addr;
-    int n;
 
-    int x[10] = {1,101,201,301,401,501,601,701,801,901};
-    int y[10] = {100,200,300,400,500,600,700,800,900,1000};
+    packet p;
+    byte buffer[256];
 
-    unsigned char mem[1000];
-
-    for(int i = 0; i < 1000;i++)
-    {
-        mem[i] = i;
-    }
+    int x[10] = {0,32,64,96,128,170,192,224,256,288};
+    int y[10] = {31,63,95,127,169,191,223,255,287,319};
 
     if (argc < 2) {
         fprintf(stderr,"ERROR, no port provided\n");
         exit(1);
     }
 
+    SERVER_SOCKET_CODE;; // creates and binds the server socket
 
-    
-    // create a socket file descriptor 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-       error("ERROR opening socket");
-
-    bzero((char *) &serv_addr, sizeof(serv_addr));    // set server address bits to zero
-
-    portno = atoi(argv[1]);
-    serv_addr.sin_family = AF_INET; 
-    serv_addr.sin_addr.s_addr = INADDR_ANY;  // INADDR_ANY is server ip address
-    serv_addr.sin_port = htons(portno);   // htons converts port number in host byte order to a port number in network byte order.
-
-
-    // bind the server to port and host ... this steps makes it a server socket 
-    if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
-              error("ERROR on binding");
-
-
-    //while(1){
-        // start listening  
-        
+    //while(1){     /* listen continuously */
      	listen(sockfd,5);
-
-
 
 	    // accept a connection 
 	    clilen = sizeof(cli_addr);
@@ -101,45 +112,45 @@ int main(int argc, char *argv[])
         // printf("Here is the message: %s\n",buffer);
         // printf("n is : %d\n ",n);
 	    // read data 
-        unsigned char size;
-        n = read(newsockfd,&size,1);
-        if (n < 0) error("ERROR reading size from socket");
+        // unsigned char size;
+        // n = read(newsockfd,&size,1);
+        // if (n < 0) error("ERROR reading size from socket");
 
-        int32_t a=0,b=0;
-        unsigned char nonce[16],eflag;
+        // int32_t a=0,b=0;
+        // unsigned char nonce[16],eflag;
 
-        if(block_recv(newsockfd, (char*)&a, sizeof(a)) != sizeof(a)) 
+        if(block_recv(newsockfd, (char*)(&p), sizeof(p)) != sizeof(p)) 
         {
-            error("ERROR while reading a");
+            error("ERROR while reading from socket");
             // whatever error handling you need...
         }
-        a = ntohl(a);
-        printf("\na: %d\n",a);
+        p.a = ntohl(p.a);
+        printf("\na: %d\n",p.a);
         
-        if(block_recv(newsockfd, (char*)&b, sizeof(b)) != sizeof(b)) 
-        {
-            error("ERROR while reading b");
-            // whatever error handling you need...
-        }
-        b = ntohl(b);
-        printf("b: %d\n\n",b);
+        // if(block_recv(newsockfd, (char*)&b, sizeof(b)) != sizeof(b)) 
+        // {
+        //     error("ERROR while reading b");
+        //     // whatever error handling you need...
+        // }
+        p.b = ntohl(p.b);
+        printf("b: %d\n\n",p.b);
 
-        if(block_recv(newsockfd, (char*)&nonce, sizeof(nonce)) != sizeof(nonce)) 
-        {
-            error("ERROR while reading nonce");
-            // whatever error handling you need...
-        }
+        // if(block_recv(newsockfd, (char*)&nonce, sizeof(nonce)) != sizeof(nonce)) 
+        // {
+        //     error("ERROR while reading nonce");
+        //     // whatever error handling you need...
+        // }
         for(int i =0 ;i<16;i++)
-            printf("%d\n",nonce[i]);
+            printf("%d\n",p.nonce[i]);
 
-        if(block_recv(newsockfd, (char*)&eflag, sizeof(eflag)) != sizeof(eflag)) 
-        {
-            error("ERROR while reading nonce");
-            // whatever error handling you need...
-        }
-        printf("eflag: %d\n", eflag);
+        // if(block_recv(newsockfd, (char*)&eflag, sizeof(eflag)) != sizeof(eflag)) 
+        // {
+        //     error("ERROR while reading nonce");
+        //     // whatever error handling you need...
+        // }
+        printf("eflag: %d\n", p.eflag);
         
-	    n = write(newsockfd,"I got your message",18);
+	    int n = write(newsockfd,"I got your message",18);
 	    if (n < 0) error("ERROR writing to socket");
 	    close(newsockfd);
 	    close(sockfd);
--- verifier_client.cpp
+++ verifier_client.cpp
@@ -9,59 +9,47 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h> 
-#include </usr/local/include/openssl/bio.h>
-#include </usr/local/include/openssl/ssl.h>
-#include </usr/local/include/openssl/err.h>
-#include </usr/local/include/openssl/rand.h>
-#include "/usr/local/include/openssl/crypto.h"
-#include </usr/include/array.hpp>
+#include <openssl/bio.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <openssl/rand.h>
 
-namespace ajd { 
-    namespace crypto {
-    /// A convenience typedef for a 128 bit block.
-    typedef boost::array<unsigned char, 16> block;
-    /// Remove sensitive data from the buffer
-    template<typename C> void cleanse(C &c)
-    }
+#define CLIENT_SOCKET_CODE  \
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); \
+    if (sockfd < 0) \
+        error("ERROR opening socket");\
+    /*Get the port number and server */ \
+    portno = atoi(argv[2]); \
+    server = gethostbyname(argv[1]); \
+    if (server == NULL) { \ 
+        fprintf(stderr,"ERROR, no such host\n"); \
+        exit(0); \
+    } \
+    /*Initialie socket port, address and address family */ \
+    bzero((char *) &serv_addr, sizeof(serv_addr)); \
+    serv_addr.sin_family = AF_INET; \
+    bcopy((char *)server->h_addr, \
+         (char *)&serv_addr.sin_addr.s_addr, \
+         server->h_length); \
+    serv_addr.sin_port = htons(portno); \
+    /*connect to server */ \
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)  \
+        error("ERROR connecting"); 
+
+
+
+typedef unsigned char byte;
+
+struct bundle{
+    int a,b;
+    byte eflag;
+    byte nonce[16];
+    byte content_ab[32];
+    byte hmac[256];
+
 };
 
-typedef unsigned char memory;
-
-
-void key_generation()
-{
-  crypto::block key;                         // 128 bit key
-  crypto::block salt;                        // 128 bit salt
-  crypto::fill_random(salt);                 // random salt
-  crypto::derive_key(key, "password", salt); // password derived key
-  crypto::cleanse(key)                       // clear sensitive data
-}
-
-
-void message_authentication_code()
-{
-  crypto::block key;            // the hash key
-  crypto::fill_random(key);     // random key will do (for now)
-  crypto::hash h(key);          // the keyed-hash object
-  crypto::hash::value mac;      // the mac value
-  h.update("hello world!");     // add data
-  h.update("see you world!");   // more data
-  h.finalize(mac);              // get the MAC code
-  crypto::cleanse(key)          // clean senstive data
-}
-
-void showbits ( int n )
-{
-    int i, k, andmask ;
-
-    for(i =(sizeof(n)*2)-1 ; i >= 0 ; i-- )
-    {
-        andmask = 1 << i ;
-        k = n & andmask ;
-    }
-
-    k == 0 ? printf ( "0" ) : printf ( "1" ) ;
-}
+typedef struct bundle packet;
 
 void inttobuff(int32_t num, char* buff, int offset)
 {
@@ -94,50 +82,28 @@ int main(int argc, char *argv[])
     int32_t sockfd, portno, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
-    unsigned char sendBuffer[256];
-    int offset = 1; // next free byte in buffer
-    
-    int x[10] = {1,101,201,301,401,501,601,701,801,901};
-    int y[10] = {100,200,300,400,500,600,700,800,900,1000};
+    packet p; // store data into p and send_it
+    int x[10] = {0,32,64,96,128,170,192,224,256,288};
+    int y[10] = {31,63,95,127,169,191,223,255,287,319};
 
-    short r;
+    
     
     if (argc < 3) {
        fprintf(stderr,"usage %s hostname port\n", argv[0]);
        exit(0);
     }
 
+    // create and initialize socket 
+    CLIENT_SOCKET_CODE;
+    bzero((byte*)&p,sizeof(p));
 
-    //Create Socket file descriptor 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
-
-    //Get the port number and server 
-    portno = atoi(argv[2]);
-    server = gethostbyname(argv[1]);
-	if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
-
-    //Initialie socket port, address and address family 
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
-    serv_addr.sin_port = htons(portno);
-
-
-    // connect to server 
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
 
     // generate a,b boundaries 
     srand(time(NULL)); // initialize random seed 
-    r = rand() % 10;
-    printf("a: %d   b: %d\n\n",x[r],y[r]);
+    short r = rand() % 10;
+    p.a = htonl(x[r]);
+    p.b = htonl(y[r]);
+    printf("a: %d   \nb: %d\n\n",x[r],y[r]);
 
     
     // initialie openssl library
@@ -148,35 +114,31 @@ int main(int argc, char *argv[])
 
     // generate nonce
     unsigned char nonce[16];
-	int rc = RAND_bytes(nonce, sizeof(nonce));
+	int rc = RAND_bytes(p.nonce, sizeof(p.nonce));
 	unsigned long err = ERR_get_error();
-
-
 	if(rc != 1) {
-	    /* RAND_bytes failed */
-	    /* `err` is valid    */
-	} 
+	    printf("RAND_bytes failed\n");
+	}
+
     for(int i =0 ;i<16;i++)
-        printf("%d\n",nonce[i]);
+        printf("%d\n",p.nonce[i]);
 
 
     // generate exec flag 
-    unsigned char eflag = rand() % 2;
-    printf("eflag: %d\n", eflag);
+    p.eflag = rand() % 2;
+    printf("eflag: %d\n", p.eflag);
     
     // bundle the data
-    bzero(sendBuffer,sizeof(sendBuffer));
+    // inttobuff(x[r],(char*)sendBuffer,offset);
+    // offset+=4;
+    // inttobuff(y[r],(char*)sendBuffer,offset);
+    // offset+=4;
 
-    inttobuff(x[r],(char*)sendBuffer,offset);
-    offset+=4;
-    inttobuff(y[r],(char*)sendBuffer,offset);
-    offset+=4;
-
-    //snprintf((char*)sendBuffer+offset,sizeof(sendBuffer)-offset,"%d%d",htonl(x[r]),htonl(y[r]));
-    //offset+=8;
-    chararrtobuff(nonce,sizeof(nonce),sendBuffer,offset);
-    offset= offset+sizeof(nonce);
-    sendBuffer[offset++] = eflag;
+    // //snprintf((char*)sendBuffer+offset,sizeof(sendBuffer)-offset,"%d%d",htonl(x[r]),htonl(y[r]));
+    // //offset+=8;
+    // chararrtobuff(nonce,sizeof(nonce),sendBuffer,offset);
+    // offset= offset+sizeof(nonce);
+    // sendBuffer[offset++] = eflag;
     
     // n = write(sockfd,sendBuffer,strlen(sendBuffer));
     // if (n < 0) 
@@ -184,11 +146,13 @@ int main(int argc, char *argv[])
 
 
     // start sending
-    sendBuffer[0] = offset;
-    int left = offset;
+    // sendBuffer[0] = offset;
+
+
+    int left = sizeof(p);
     int wc;
     while (left) {
-        wc = write(sockfd, sendBuffer + offset - left, left);
+        wc = write(sockfd, (byte*)(&p) + sizeof(p) - left, left);
         if (wc < 0) 
             error("ERROR writing to socket");
         left -= wc;
@@ -196,6 +160,7 @@ int main(int argc, char *argv[])
 
 
     // receive data from socket 
+    byte sendBuffer[256];
     bzero(sendBuffer,sizeof(sendBuffer));
     n = read(sockfd,sendBuffer,sizeof(sendBuffer) -1 );
     if (n < 0) 
